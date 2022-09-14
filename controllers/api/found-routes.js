f7849173e284fa8847790899535b9e4ee6679509
paddybarn <107075686+paddybarn@users.noreply.github.com>
2022-09-14 14:19:27 -0400

    added post route for found pets

--- controllers/api/found-routes.js
+++ controllers/api/found-routes.js
@@ -1,6 +1,25 @@
 const router = require('express').Router();
-const { Found } = require('../../models');
+const { Found } = require('../../models')
 
+router.post('/', async (req, res) => {
+    try {
+        const foundPet = await Found.create({
+            id: req.body.id,
+            name: req.body.name,
+            weight: req.body.weight,
+            breed: req.body.breed,
+            sex: req.body.sex,
+            fixed: req.body.fixed,
+            color: req.body.color,
+            dateLost: req.body.dateLost,
+        })
+        res.status(200).json(foundPet);
+
+        } catch (err) {
+        console.log(err);
+        res.status(404).json(err);
+    }
+  });
 
 
 
