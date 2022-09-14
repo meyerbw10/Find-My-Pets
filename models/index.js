c20bd65e7fa50800fd8db8ad7b926e0f6bea24de
paddybarn <107075686+paddybarn@users.noreply.github.com>
2022-09-14 11:32:18 -0400

    saving changes

--- models/index.js
+++ models/index.js
@@ -1,14 +1,12 @@
 const User = require('./User');
-const Lost = require('./Lost');
-const Found = require('./Found');
-const Animal = require('./Animal');
 
-Gallery.hasMany(Painting, {
-  foreignKey: 'animal_id',
-});
 
-Painting.belongsTo(Gallery, {
-  foreignKey: 'animal_id',
-});
+// Gallery.hasMany(Painting, {
+//   foreignKey: 'animal_id',
+// });
 
-module.exports = { User, Found , Lost, Animal};
+// Painting.belongsTo(Gallery, {
+//   foreignKey: 'animal_id',
+// });
+
+module.exports = { User};
