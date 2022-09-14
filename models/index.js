8ea54e0eff90aaff7dcda34f0b613fac4e658eca
meyerbw10 <109110828+meyerbw10@users.noreply.github.com>
2022-09-14 11:30:20 -0400

    fixing

--- models/index.js
+++ models/index.js
@@ -1,14 +1,4 @@
 const User = require('./User');
-const Lost = require('./Lost');
-const Found = require('./Found');
 const Animal = require('./Animal');
 
-Gallery.hasMany(Painting, {
-  foreignKey: 'animal_id',
-});
-
-Painting.belongsTo(Gallery, {
-  foreignKey: 'animal_id',
-});
-
-module.exports = { User, Found , Lost, Animal};
+module.exports = { User, Animal };
