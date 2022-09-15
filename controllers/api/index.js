eda420d1071bcef1f4e93ae5c6099213ffcb1ba8
Mersant <dlowry99@outlook.com>
2022-09-15 11:05:00 -0400

    Added functioning GET and POST routes for lost and found

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
-const foundRoutes = require('./found-routes')
+const foundRoutes = require('./found-routes');
+const lostRoutes = require('./lost-routes');
 
 router.use('/users', userRoutes);
-router.use('/found', foundRoutes)
+router.use('/found', foundRoutes);
+router.use('/lost', lostRoutes);
 
 module.exports = router;
